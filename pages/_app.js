cd6df3a8192b4decefd139b9f6f099c80e29b9ca
Majd- <majd.abazid@gmail.com>
2022-04-01 08:43:12 +0200

    :white_check_mark: add nextjs Head(title/desc) for search engine

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
 import DenseAppBar from '../components/layout/header';
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <DenseAppBar>
-      <Component { ...pageProps } />
-    </DenseAppBar>
+    <Fragment>
+      <Head>
+        <title>Worlds Holidays</title>
+        <meta
+          name="description"
+          content="Get to know world wide countries public holidays!"
+        />
+      </Head>
+      <DenseAppBar>
+        <Component { ...pageProps } />
+      </DenseAppBar>
+    </Fragment>
   );
 }
 
